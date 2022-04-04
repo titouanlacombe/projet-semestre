cfab2d8485dc0d472e590d36635ba82501f61c18
titouanlacombe <titouan.lacombe99@gmail.com>
2022-04-04 11:37:45 +0200

    refactored file explorer

--- app/Sidebar.js
+++ app/Sidebar.js
@@ -1,15 +1,51 @@
-async function appendFiles(parent, folder)
+const separator = "fileExplorer://";
+
+function folderOpened(folder)
 {
+	return JSON.parse(document.getElementById(separator + folder).dataset.opened);
+}
+
+async function appendFiles(folder)
+{
+	// Get parent and add after wrapper list
+	const list = document.createElement("ul");
+	const parent = document.getElementById(separator + folder)
+	parent.after(list);
+	parent.dataset.opened = "true";
+
 	const files = await window.electronAPI.getFiles(folder);
 
 	for (const file of files) {
-		let child = parent.appendChild(document.createElement("li"));
+		let filePath = folder + "/" + file;
+
+		let child = list.appendChild(document.createElement("li"));
+
 		child.innerText = file;
 		child.className = "file";
+		child.id = separator + filePath;
+		child.dataset.opened = "false";
+		child.onclick = () =>
+		{
+			// Precondition
+			if (folderOpened(filePath)) {
+				return;
+			}
+
+			appendFiles(filePath);
+		};
 	}
+}
+
+function initFileExplorer()
+{
+	const rootFolder = ".";
+	const root = document.getElementById(separator);
+
+	root.id += rootFolder;
+	appendFiles(rootFolder);
 }
 
 window.addEventListener('DOMContentLoaded', () =>
 {
-	appendFiles(document.getElementById("filesRoot"), ".");
+	initFileExplorer();
 });
