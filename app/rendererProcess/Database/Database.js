c21c471ae8277bdb3be8742a1bfbd1fdae333192
titouanlacombe <titouan.lacombe99@gmail.com>
2022-05-03 16:08:55 +0200

    bloop

--- app/rendererProcess/Database/Database.js
+++ app/rendererProcess/Database/Database.js
@@ -1,17 +1,21 @@
 export class Database
 {
 	// TODO implement
+	// TODO https://www.sqlitetutorial.net/
 	// Create tables & insert static data
 	static async seedDatabase()
 	{
 		console.log("Seeding database...");
 
-		let result = await window.electronAPI.runSQL("run", `
+		await window.electronAPI.runSQL("run", `
 			CREATE TABLE albums (
 				id INTEGER PRIMARY KEY,
-				name TEXT NOT NULL,
+				name TEXT NOT NULL
 			);
-			INSERT INTO albums VALUES ("Mezzanine");
+		`, []);
+
+		await window.electronAPI.runSQL("run", `
+			INSERT INTO albums VALUES ('Mezzanine');
 		`, []);
 
 		console.log("Seeding complete, results:");
