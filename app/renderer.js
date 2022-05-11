9b8a3c399bb760184046916240814b858150624e
titouanlacombe <titouan.lacombe99@gmail.com>
2022-05-11 16:05:41 +0200

    bloop

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,3 @@
 import { } from "./rendererProcess/Components/Sidebar/FileExplorer.js";
-import { } from "./rendererProcess/Database/Database.js";
+import { Database } from "./rendererProcess/Database/Database.js";
 import { } from "./rendererProcess/Components/ImportButton/ImportButton.js";
