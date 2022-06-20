5e8757ebb5faa30337fb738232bb09dc9c3fdcd2
Nathan BARBIER <nathang.barbier@gmail.com>
2022-06-20 09:44:21 +0200

    Artist creation

--- app/rendererProcess/Models/Artist.js
+++ app/rendererProcess/Models/Artist.js
@@ -71,4 +71,73 @@ export class Artist extends Model
     {
         return Band.find(this.band_id);
     }
+
+    async create()
+    {
+
+        let keys = Object.keys(this);
+        for (const key of keys) {
+            if (this[key] === '' || this[key] === null || this[key] === undefined) {
+                this[key] = 'null';
+            }
+        }
+
+        Database.sql(`
+			INSERT INTO artists (firstname, lastname, stagename, band_id)
+            VALUES ('${this.firstname}', '${this.lastname}', '${this.stagename}', ${this.band_id});`
+        );
+    }
+
+    async createArtist()
+    {
+        console.log(this);
+
+        if (!this) {
+            return null;
+        }
+
+        let stopError = false;
+        let data = false;
+
+        if (this.stagename) {
+            data = true;
+
+        } else if (this.firstname || this.lastname) {
+            data = true;
+            this.stagename = `${this.firstname} ${this.lastname}`;
+        }
+        else {
+            alert("Veuillez entrer un nom.");
+        }
+
+
+        // get band_id from db
+        if (this.band_id) {
+            let band = await Band.searchid(this.band_id);
+            if (band) {
+                this.band_id = band.rowid;
+            }
+            else {
+                stopError = true;
+                this.band_id = null;
+                alert("Aucun groupe correspondant trouvé, veuillez le créer au préalable.");
+            }
+        }
+
+        if (stopError === false && data === true) {
+            document.getElementsByClassName("container")[0].remove();
+            try {
+                await this.create();
+                alert("Artiste créé avec succès.");
+            }
+            catch (error) {
+                alert("Erreur lors de la création de l'artiste.");
+            }
+        }
+        else if (this.firstname || this.stagename) {
+            alert("Erreur lors de la création de l'artiste.\nVeuillez vérifier les données entrées.");
+        }
+
+        return null;
+    }
 }
