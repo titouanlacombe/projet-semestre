0adfea6a26360655769e38b94b7e6564bfa48b13
Nathan BARBIER <nathang.barbier@gmail.com>
2022-06-22 15:03:11 +0200

    Added rowid to select function in Model

--- app/rendererProcess/Models/Model.js
+++ app/rendererProcess/Models/Model.js
@@ -2,60 +2,60 @@ import { Database } from "../Database/Database.js";
 
 export class Model
 {
-	static table = undefined;
+    static table = undefined;
 
-	static async find(id)
-	{
-		return this.get(`WHERE _rowid_ = ${id}`, []);
-	}
+    static async find(id)
+    {
+        return this.get(`WHERE _rowid_ = ${id}`, []);
+    }
 
-	static generateModel(object)
-	{
-		if (!object) {
-			return null;
-		}
+    static generateModel(object)
+    {
+        if (!object) {
+            return null;
+        }
 
-		let model = new this();
-		for (const key in object) {
-			model[key] = object[key];
-		}
-		return model;
-	}
+        let model = new this();
+        for (const key in object) {
+            model[key] = object[key];
+        }
+        return model;
+    }
 
-	static async get(request, params)
-	{
-		return this.generateModel(await this.select(request, params, 'get'));
-	}
+    static async get(request, params)
+    {
+        return this.generateModel(await this.select(request, params, 'get'));
+    }
 
-	static async all(request, params)
-	{
-		let objects = await this.select(request, params, 'all');
+    static async all(request, params)
+    {
+        let objects = await this.select(request, params, 'all');
 
-		let results = [];
-		for (const iterator of objects) {
-			results.push(this.generateModel(iterator));
-		}
+        let results = [];
+        for (const iterator of objects) {
+            results.push(this.generateModel(iterator));
+        }
 
-		return results;
-	}
+        return results;
+    }
 
-	static async select(request, params, method)
-	{
-		return Database.sql(`SELECT * FROM ${this.table} ${request}`, params, method);
-	}
+    static async select(request, params, method)
+    {
+        return Database.sql(`SELECT ROWID, * FROM ${this.table} ${request}`, params, method);
+    }
 
-	static async create(object)
-	{
-		let keys = Object.keys(object);
+    static async create(object)
+    {
+        let keys = Object.keys(object);
 
-		let values = [];
-		for (const key of keys) {
-			values.push(object[key]);
-		}
+        let values = [];
+        for (const key of keys) {
+            values.push(object[key]);
+        }
 
-		Database.sql(`
+        Database.sql(`
 			INSERT INTO ${this.table}(${keys.join(", ")})
 			VALUES (${values.join(", ")})
 		`);
-	}
+    }
 }
